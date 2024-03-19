0225a7373367d3623113d73a959857f698553fa2
SS4nd <silas.sandager@gmail.com>
2024-03-19 13:14:08 +0100

    changed url so it uses id as params and is ready for backend

--- src/SeatSelectorPage.jsx
+++ src/SeatSelectorPage.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import SeatSelector from './components/SeatSelector';
 import './SeatSelectorPage.css'; 
+import { useParams } from 'react-router-dom';
 
 const SeatSelectorPage = () => {
+    const { id } = useParams(); 
+
+    //useAxios Fetch tickets with id as id
+    var numRows = 20;
+    var numSeatsPerRow = 12;
+    var seatsStatus = "none";
+
+
     const [selectedSeats, setSelectedSeats] = useState([]);
     const [showPopup, setShowPopup] = useState(false);
 
@@ -23,7 +32,7 @@ const SeatSelectorPage = () => {
     return (
         <div>
             <h1>Select Seats</h1>
-            <SeatSelector numRows={20} numSeatsPerRow={12} seatsStatus={"none"} onSeatSelect={handleSeatSelect} />
+            <SeatSelector numRows={numRows} numSeatsPerRow={numSeatsPerRow} seatsStatus={seatsStatus} onSeatSelect={handleSeatSelect} />
             
             {showPopup && (
                 <div className="popup slideout-right">
